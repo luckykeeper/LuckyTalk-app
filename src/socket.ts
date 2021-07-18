9e22b73d313d349d10fe7d62419719fe14e9ec53
Luckykeeper <2571224985@qq.com>
2021-07-19 03:27:57 +0800

    Update socket.ts

--- src/socket.ts
+++ src/socket.ts
@@ -38,7 +38,7 @@ const options = {
     transports: ['websocket'],
 };
 
-const host = 'http://ipv4.luckykeeper.site:9200/';
+const host = 'http://ipv4.luckykeeper.site:9200';
 const socket = IO(host, options);
 
 function fetch<T = any>(
